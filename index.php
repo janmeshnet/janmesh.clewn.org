7e72d34cd250ee4e4b582ae6378b834b8b2cef6b
Nico <nicolas.chartoire@gmail.com>
2018-06-12 13:45:51 +0200

    full re-coding of the whole site

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Janmesh - Shaping the networks</title>
+ <meta charset="UTF-8"> 
+ <style>
+ .mainimg{
+	 width:40%;
+	 margin-left:30%;
+	 margin-right:30%;
+ }
+ body {
+	 font-family:sans-serif;
+	 margin-left:8%;
+	 margin-right:8%;
+	 text-align:left;
+ }
+ div  {
+	 font-family:sans-serif;
+	 margin-top:3%;
+	 margin-left:3%;
+	 margin-right:3%;
+ }
+ .topmenu {
+	 background-color:grey;
+	 margin-top:2%;
+	padding: 4px;
+	 }
+	 	a {
+		text-decoration:none;
+	}
+
+ </style>
+</head>
+<body>
+	<div style="width:100%;border: solid black 3px; border-radius: 8px;font-size:84%;">
+	<em>Need support?</em> To help people setting up mesh stations, Janmesh community now provides support, covering all the steps of a neighbour-to-neighbour network creation, from OS installation to configuration and update, including CoOoOw stack deployment. Send support inquiries to <a href="mailto:janmesh@clewn.org">janmesh@clewn.org</a><br/><span style="font-size:78%;">You can also join the IRC live chat ; #janmesh channel on Freenode IRC network namely. If you don't get what all this mean, use <a target="new" href="https://kiwiirc.com/nextclient/irc.freenode.net/#janmesh">this webchat link</a> to get into the live chat chatroom</span>
+	
+	</div>
+<span style="text-align:center;">
+<h1>Janmesh.clewn.org</h1>
+<h3>Joint Access Mesh Networks</h3>
+</span>
+<h2>What is Janmesh? What is a Janmesh box? </h2>
+<strong>Janmesh</strong> (SSID http://janmesh.clewn.org) is a citizen-to-citizen networking initiative, running nodes with an active <strong>CoOoOW</strong> stack (<em>cjdns</em> over <em>OLSRD</em> over open WiFi), that provide an alternative communication medium resistant to severely communication network-disruptive situations. <br/>
+<strong>A Janmesh box</strong> is a computing device (typically an old laptop or a cheap one SoC/SbC computing device) running as a Janmesh network node, connected to neighbourhood's nodes, and serving mesh connectivity and services toward the home LAN network<br/>
+<div style="float;right;">
+<h2>How is the Janmesh Project doing?</h2>
+A full documentation to get a working CoOoOW stack with basic Ubuntu GNU/Linux computers is available. The network is up and runing at a local scale<br>
+<h2>Documentation and tutorials</h2>
+<h3>Beginner</h3>
+Read how to connect two machines in mesh mode, using their wifi links and a CoOoOw stack, from scratch, using GUI tools ; or, to add a new machine to your existing mesh network using command line. Then, learn more about firefall to secure (network) access to your devices.  <a href="index-main.php">-Read more-</a>.
+<br/>
+<h3>Intermediate</h3>
+Lear how to use one machine that operate Janmesh over Wifi and that is connected to Internet through Ethernet to act as a gateway and NAT box in order to allow other devices with (meshing) wifi only to share its Internet access.  <a href="intermediate.php">-Read more -</a>
+<hr/>
+</div>
+<h2 style="clear:both;">And now learn more about citizen-to-citizen networking future...</h2>
+<h2>The connectivity to the mesh network can ben tunneled through a long-range directionnal radio link to connect distant communities</h2>
+<h3>Or tunneled over Internet -or any other network medium- to rely distant places of the world</h3>
+<a href="index-main.html">Get started</a> or read more:
+<h1>Frequently asked questions</h1>
+<h2>What can I do with a Janmesh in my neighbourhood already? Do I have to wait for further developments to make something useful?</h2>
+In short, you don't have to wait. If you have physical or remote access to your Janmesh node station, you can just use out-of-the-box any network services such as SMTP mails, HTTP/FTP file transfer or just any other common protocol, with the requirement that you'll have to know the IPv6 adress of the machine you'll want to talk to. 
+<h2>Is Janmesh using only radio link? Can I use some other kind of networking medium? </h2>
+You can use any networking medium to setup an operate your CoOoOw stacks. You can use an ethernet cable to connect with a close neighbour, as well as associated BPL interfaces to connect with other homes using the same electrical power transformator than you (typically, your block and nearest ones). 
+<h2>Why Janmesh? Why spend your sweat and energy to propel the whole project?</h2>
+I'm not sure. I'm having fun with experimenting neigbourhood networking since the early 2000's. I just cannot prevent myself to stop. I was hoping my close neighbours would read all this and mesh with me, to aae share files or text messages or built whatever useful. This hasn't happened. To speak frankly, I think that the bricks layed here can only be useful in case of severely disruptive conditions, like say, useful to <strong>survivalists</strong> to get <strong>post-apocalyptic communication means</strong> to link communities. I don't believe frankly in such a scenario on my own, but if this can be useful, I'm happy to help. (<em>Shangri-l on June '18</em>)
+<h1>Donate</h1>
+<?php include ('../bank/index.php'); ?>
+</body>
+</html>
